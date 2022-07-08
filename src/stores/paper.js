081a9f1b926084205bda03c8ec66d5203a99b266
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-07-08 16:49:11 +0900

    fixed

--- src/stores/paper.js
+++ src/stores/paper.js
@@ -7,8 +7,47 @@ export const usePaperStore = defineStore('paper', () => {
     width: 0,
     heigth: 0,
   });
+  const block = reactive({
+    width: 0,
+    heigth: 0,
+  });
+  const gap = reactive({
+    x: 0,
+    y: 0,
+  });
+  const margin = reactive({
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  });
+
+  const gapX = () => {
+    return paper.width - block.width;
+  };
+  const gapY = () => {
+    return paper.heigth - block.heigth;
+  };
+
+  const adjustPaperWidth = (width) => {
+    paper.width = width;
+
+    const gapX = width - block.width;
+    gap.X = gapX;
+    margin.left = gap / 2;
+    margin.right = gap / 2;
+  };
+  const adjustBlockWidth = (width) => {
+    block.width = width;
+
+    const gapX = paper.width - width;
+    gap.X = gapX;
+    margin.left = gap / 2;
+    margin.right = gap / 2;
+  };
 
   return {
     paper,
+    block,
   };
 });
